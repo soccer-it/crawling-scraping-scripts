a2c77471bb289365bcb43e279baf515991c5c3ab
Victor Igor <victor.0w3@gmail.com>
2019-01-16 20:47:07 -0200

    feat: getting all teams from serie a and b

--- teams/list/src/main.rs
+++ teams/list/src/main.rs
@@ -4,11 +4,11 @@ use std::io::Read;
 use select::document::Document;
 use select::predicate::{Class, Name, Predicate};
 
-fn crawl(url: &str) {
+fn crawl(url: &str) ->  Vec<String> {
     let mut response = reqwest::get(url)
         .expect("Failed to send request");
 
-    println!("Getting data from {} ...", url);
+    println!("Getting data from {}", url);
     let mut body = String::new();
     response.read_to_string(&mut body).expect("Failed to read response");
 
@@ -16,16 +16,26 @@ fn crawl(url: &str) {
 
     println!("Finding nodes...");
 
-    let teams_serie_a = document
+    let teams_from_serie = document
         .find(Class("table").descendant(Name("a")))
         .map(|name| name.text())
         .collect::<Vec<_>>();
 
-    println!("Serie A:\n");
-    println!("{}", teams_serie_a.join("\n"))
+    return teams_from_serie
 }
 
 pub fn main() {
-    let url = "https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-a/".to_string();
-    crawl(&url)
+    let teams_series = vec!["a", "b"];
+    let all_teams = teams_series
+        .iter()
+        .fold(vec![], |mut acc, serie| {
+            let url = format!("https://www.gazetaesportiva.com/campeonatos/brasileiro-serie-{}/", serie);
+            acc.push(crawl(&url));
+            acc
+        })
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>();
+
+    println!("All teams: {:?}", all_teams);
 }
