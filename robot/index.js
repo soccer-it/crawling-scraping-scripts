1e4d1d118466a44017b6ac64c98f40657de3956e
Milson Ramos de Carvalho Júnior <junior.milson17@gmail.com>
2019-04-20 05:35:26 -0300

    feat: add base structure for campeonato brasileiro 2019 scraping

--- robot/index.js
+++ robot/index.js
@@ -1,27 +1,36 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+const config = require("../config.json");
+const argv = require("minimist")(process.argv.slice(2));
 
-const siteUrl = "https://globoesporte.globo.com/futebol/brasileirao-serie-a/";
-const scriptUrl =
-  "https://soccer-it-crawling-scraping.mjnr.now.sh/output/campeonato-brasileiro-2019/teams.js";
+const { scraper } = argv;
+
+const { siteUrl, scriptUrl } = config.scrapers[scraper];
 
 (async () => {
+  console.log("Scraping data ...");
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(siteUrl);
 
   await page.addScriptTag({ url: scriptUrl });
 
-  const data = await page.evaluate(() => window.getTeams());
+  const data = await page.evaluate(() => window.scrapeData());
 
-  fs.writeFile("./data.json", JSON.stringify(data), err => {
-    if (err) {
-      console.error("[ Robot ] - Error on write scraper result");
-    } else {
-      console.log("[ Robot ] - Date written!");
+  fs.writeFile(
+    `./data_${scraper}_${new Date().getTime()}.json`,
+    JSON.stringify(data),
+    err => {
+      if (err) {
+        console.error("[ Robot ] - Error on write scraper result");
+      } else {
+        console.log("[ Robot ] - Date written!");
+      }
     }
-  });
+  );
 
+  console.log("Done!");
   await browser.close();
 
   process.exit();
