d2915ab4603dea0948c420a0b25e31469fb210b6
Milson Ramos de Carvalho Júnior <junior.milson17@gmail.com>
2019-04-27 22:15:55 -0300

    feat: add getConfig helper

--- app/helpers/index.js
+++ app/helpers/index.js
@@ -6,3 +6,10 @@ import reduce from "ramda/src/reduce";
 export const getTextContent = prop("textContent");
 export const indexedMap = addIndex(map);
 export const indexedReduce = addIndex(reduce);
+export const getClassList = prop("classList");
+export const getConfig = () => {
+  const SCRAPER = process.env.SCRAPER;
+  const config = require("../../config");
+
+  return config.scrapers[SCRAPER];
+};
